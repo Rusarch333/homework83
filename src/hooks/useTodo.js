a68be98fbdef6e77a119fd9e338dca3d424db334
Lite <rusarch333@gmail.com>
2024-07-28 15:25:10 +0300

    Add task

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,9 +1,20 @@
 import { useState } from 'react';
+import {v4 as uuidv4 } from 'uuid';
 
 const useTodo = (initialTasks) => {
   const [tasks, setTasks] = useState([...initialTasks]);
 
-  return { tasks, setTasks };
+  const addTask = (task) => {
+    const newTask = {
+      id: uuidv4(),
+      content: task.content,
+      isDone: false,
+    };
+    // Передаем сначала все таски, иначе наша новая таска просто сотрет прошлые таски, и потом уже новую таску
+    setTasks([...tasks, newTask]);
+  }
+
+  return { tasks, setTasks, addTask };
 };
 
 export default useTodo;
